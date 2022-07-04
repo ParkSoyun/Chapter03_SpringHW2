79a75188aeaf9ccbbeec35233c0abdec8a45fdc7
ParkSoyun <soyp@naver.com>
2022-07-04 14:49:37 +0900

    ADD: initial files and .gitignore

---
+++ src/main/java/com/sparta/springhw1/SpringHw1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springhw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springhw1/SpringHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springhw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
