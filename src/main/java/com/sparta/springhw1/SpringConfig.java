5f0f646581556b928fe52c5682c74603e34c7c2f
ParkSoyun <soyp@naver.com>
2022-07-07 15:15:05 +0900

    CREATE: PATCH, DELETE 사용을 위해 HiddenHttpMethodFilter클래스 Bean 등록

---
+++ src/main/java/com/sparta/springhw1/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.sparta.springhw1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public HiddenHttpMethodFilter httpMethodFilter() {
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        return hiddenHttpMethodFilter;
+    }
+}
