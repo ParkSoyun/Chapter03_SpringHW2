dcb3a2d705a6009b0e3b416fa8dd33713ca6ca9f
ParkSoyun <soyp@naver.com>
2022-07-07 15:18:10 +0900

    CREATE: repository 생성

---
+++ src/main/java/com/sparta/springhw1/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.springhw1.repository;
+
+import com.sparta.springhw1.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreatedAtDesc();
+}
