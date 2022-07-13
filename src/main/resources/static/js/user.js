e951b817ca7587a865d677c44fd6d95b6dd073e6
ParkSoyun <soyp@naver.com>
2022-07-14 03:02:19 +0900

    ADD: JS 파일 추가

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,58 @@
+// (function() {
+//     "use strict";
+//     window.addEventListener("load", function() {
+//         var form = document.getElementById("needs-validation");
+//
+//         form.addEventListener("button", function(event) {
+//             if (!form.checkValidity()) {
+//                 event.preventDefault();
+//                 event.stopPropagation();
+//             }
+//                 form.classList.add("was-validated");
+//
+//         }, false);
+//     }, false);
+// }());
+
+function checkExistId(id) {
+    $.ajax({
+        type: "GET",
+        url: "/users/signup/" + id,
+        success: function (response) {
+            if(!response) {
+                if(document.getElementById('id').classList.contains('is-invalid')) {
+                    document.getElementById('id').classList.replace('is-invalid', 'is-valid');
+                } else {
+                    document.getElementById('id').classList.add('is-valid');
+                }
+
+                $('#check-duplication-success').attr("hidden", false);
+                $('#check-duplication-fail').attr("hidden", true);
+            } else {
+                if(document.getElementById('id').classList.contains('is-valid')) {
+                    document.getElementById('id').classList.replace('is-valid', 'is-invalid');
+                } else {
+                    document.getElementById('id').classList.add('is-invalid');
+                }
+
+                $('#check-duplication-fail').attr("hidden", false);
+                $('#check-duplication-success').attr("hidden", true);
+            }
+
+        }
+    });
+}
+
+function checkDuplicationResult() {
+    if(document.getElementById('id').classList.contains('is-valid')) {
+        return true;
+    } else if(document.getElementById('id').classList.contains('is-invalid')) {
+        alert("이미 존재하는 아이디입니다.");
+
+        return false;
+    } else {
+        alert("아이디 중복 체크를 해주세요.");
+
+        return false;
+    }
+}
