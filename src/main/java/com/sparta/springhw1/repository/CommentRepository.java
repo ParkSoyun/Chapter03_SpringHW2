b7489b5b5210dff2fd007c23161d9cc1d8a4b897
ParkSoyun <soyp@naver.com>
2022-07-15 20:38:59 +0900

    ADD: 댓글 관련 service 추가

--- src/main/java/com/sparta/springhw1/repository/CommentRepository.java
+++ src/main/java/com/sparta/springhw1/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.springhw1.repository;
 
 import com.sparta.springhw1.domain.Comment;
+import com.sparta.springhw1.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPostOrderByCreatedAtAsc(Post post);
 }
---
+++ src/main/java/com/sparta/springhw1/service/CommentService.java
@@ -0,0 +1,50 @@
+package com.sparta.springhw1.service;
+
+import com.sparta.springhw1.domain.Comment;
+import com.sparta.springhw1.domain.Post;
+import com.sparta.springhw1.dto.InsertCommentRequestDto;
+import com.sparta.springhw1.dto.UpdateCommentRequestDto;
+import com.sparta.springhw1.repository.CommentRepository;
+import com.sparta.springhw1.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public List<Comment> getCommentList(Post post) {
+        List<Comment> comments = commentRepository.findAllByPostOrderByCreatedAtAsc(post);
+
+        return comments;
+    }
+
+    public Long getPostId(Long commentId) {
+        Long postId = commentRepository.findById(commentId).get().getPost().getPostId();
+
+        return postId;
+    }
+
+    public Long saveComment(Post post, UserDetailsImpl user, InsertCommentRequestDto insertCommentRequestDto) {
+        Comment comment = new Comment(post, user, insertCommentRequestDto);
+
+        commentRepository.save(comment);
+
+        return comment.getCommentId();
+    }
+
+    public void modifyComment(Long commentId, UpdateCommentRequestDto updateCommentRequestDto) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+
+        comment.update(updateCommentRequestDto);
+    }
+
+    public void deleteComment(Long commentId) {
+        commentRepository.deleteById(commentId);
+    }
+}
