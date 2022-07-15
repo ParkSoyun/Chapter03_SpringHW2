2e21183fe1778c18d44938d8f5a611934b5e30b5
ParkSoyun <soyp@naver.com>
2022-07-15 20:40:41 +0900

    ADD: 댓글 관련 controller 추가

---
+++ src/main/java/com/sparta/springhw1/controller/CommentController.java
@@ -0,0 +1,47 @@
+package com.sparta.springhw1.controller;
+
+import com.sparta.springhw1.domain.Comment;
+import com.sparta.springhw1.domain.Post;
+import com.sparta.springhw1.dto.InsertCommentRequestDto;
+import com.sparta.springhw1.dto.UpdateCommentRequestDto;
+import com.sparta.springhw1.security.UserDetailsImpl;
+import com.sparta.springhw1.service.CommentService;
+import com.sparta.springhw1.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+public class CommentController {
+    private final PostService postService;
+    private final CommentService commentService;
+
+    @PostMapping("/comments/{postId}")
+    public String saveComment(@PathVariable("postId") Long postId, @AuthenticationPrincipal UserDetailsImpl user, InsertCommentRequestDto insertCommentRequestDto) {
+        Post post = postService.getPost(postId);
+
+        commentService.saveComment(post, user, insertCommentRequestDto);
+
+        return "redirect:/posts/" + postId;
+    }
+
+    @PatchMapping("/comments/{commentId}")
+    @ResponseBody
+    public Long modifyComment(@PathVariable("commentId") Long commentId, UpdateCommentRequestDto updateCommentRequestDto) {
+        commentService.modifyComment(commentId, updateCommentRequestDto);
+
+        return commentId;
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    @ResponseBody
+    public Long deleteComment(@PathVariable("commentId") Long commentId) {
+        Long postId = commentService.getPostId(commentId);
+
+        commentService.deleteComment(commentId);
+
+        return commentId;
+    }
+}
